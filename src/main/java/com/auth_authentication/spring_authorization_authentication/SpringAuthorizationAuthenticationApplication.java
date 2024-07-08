6b8dd29a5613cf1decef558c95d1efc11ce8d2a5
Vivek <agraharivivek13@gmail.com>
2024-07-08 13:35:57 +0530

    Initial Commit

---
+++ src/main/java/com/auth_authentication/spring_authorization_authentication/SpringAuthorizationAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.auth_authentication.spring_authorization_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth_authentication/spring_authorization_authentication/SpringAuthorizationAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_authentication.spring_authorization_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
